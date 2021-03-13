75e4e4a0605c425206afc77132e7afb1810c42e7
Facundo Torres <ftorres@ubicuagroup.com>
2021-03-13 02:37:25 -0300

    prueba 2

--- server.js
+++ server.js
@@ -1,10 +1,16 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
+//Install express server
 const express = require('express');
+const path = require('path');
+
 const app = express();
-app.use(requireHTTPS);
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/turnera'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/turnera/index.html'));
+});
+
+// Start the app by listening on the default Heroku port
+app.listen(process.env.PORT || 4200);
