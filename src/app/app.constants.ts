0899ef8e460e35e4ea8fdc300197dcb0ea100ff1
Facundo Torres <ftorres@ubicuagroup.com>
2021-03-25 20:13:30 -0300

    updated constants

--- src/app/app.constants.ts
+++ src/app/app.constants.ts
@@ -1,2 +1,2 @@
-// export const SERVER_API_URL = "https://api-turnera.herokuapp.com/";
-export const SERVER_API_URL = "http://127.0.0.1:8080/";
+export const SERVER_API_URL = "https://api-turnera.herokuapp.com/";
+// export const SERVER_API_URL = "http://127.0.0.1:8080/";
