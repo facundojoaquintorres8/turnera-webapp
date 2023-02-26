d9d639d7ca26d03189c109359f2d8a475b574dca
Facundo Torres <ftorres@ubicuagroup.com>
2023-02-26 03:49:50 -0300

    WIP

--- src/app/shared/date-format.ts
+++ src/app/shared/date-format.ts
@@ -1,24 +1,36 @@
 import { NgbDateStruct, NgbTimeStruct } from "@ng-bootstrap/ng-bootstrap";
 
-export function formatDateFromNgbDateStruct(date: NgbDateStruct): string {
+export function formatDateFromNgbDateStruct(date: NgbDateStruct): string | null {
+    if (!date) {
+        return null;
+    }
     return date.year + '-' + formatNumberToTwoDigits(date.month) + '-'
         + formatNumberToTwoDigits(date.day);
 }
 
-export function formatDateFromDate(date: Date): string {
+export function formatDateFromDate(date: Date): string | null  {
+    if (!date) {
+        return null;
+    }
     return date.getFullYear() + '-' + formatNumberToTwoDigits(date.getMonth() + 1) + '-'
         + formatNumberToTwoDigits(date.getDate());
 }
 
-export function formatTimeFromNgbTimeStruct(time: NgbTimeStruct): string {
+export function formatTimeFromNgbTimeStruct(time: NgbTimeStruct): string | null  {
+    if (!time) {
+        return null;
+    }
     return formatNumberToTwoDigits(time.hour) + ':'
         + formatNumberToTwoDigits(time.minute) + ':' + formatNumberToTwoDigits(time.second);
 }
 
-function formatNumberToTwoDigits(number: number) {
-    return ("00" + number).slice(-2);
+function formatNumberToTwoDigits(x: number): string {
+    return ("00" + x).slice(-2);
 }
 
-export function formatNgbDateStructFromDate(date: Date): NgbDateStruct {
+export function formatNgbDateStructFromDate(date: Date): NgbDateStruct | null {
+    if (!date) {
+        return null;
+    }
     return { year: date.getFullYear(), month: date.getMonth() + 1, day: date.getDate() };
 }
